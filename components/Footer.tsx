2a8deca891a551e1fc6bd94a9404a77e20692422
marciobever <bevervansomarcio@gmail.com>
2025-10-27 22:38:12 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,8 @@
 // components/Footer.tsx
+'use client'
+
 import Link from 'next/link'
+import type { Route } from 'next'
 import Logo from '@/components/Logo'
 
 export default function Footer() {
@@ -19,9 +22,9 @@ export default function Footer() {
 
           {/* Links essenciais */}
           <nav className="footer-links">
-            <Link href="/privacidade">Política de Privacidade</Link>
-            <Link href="/sobre">Quem somos</Link>
-            <Link href="/termos">Termos</Link>
+            <Link href={'/privacidade' as Route}>Política de Privacidade</Link>
+            <Link href={'/sobre' as Route}>Quem somos</Link>
+            <Link href={'/termos' as Route}>Termos</Link>
           </nav>
         </div>
       </div>
