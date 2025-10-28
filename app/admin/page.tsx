ddc3aae765cf6b79c272fa1ea95d57babff593fa
marciobever <bevervansomarcio@gmail.com>
2025-10-28 16:26:05 +0000

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,10 @@
-import AdminDashboard from "./AdminDashboard";
-export default function Page() {
+// app/admin/page.tsx
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+export const revalidate = 0;
+
+import AdminDashboard from './AdminDashboard';
+
+export default async function Page() {
   return <AdminDashboard />;
 }
