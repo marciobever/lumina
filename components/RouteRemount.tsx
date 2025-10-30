43b14ac3a004fca9b29e606006bbc730aec03921
marciobever <bevervansomarcio@gmail.com>
2025-10-30 08:53:33 +0000

    Create RouteRemount.tsx

---
+++ components/RouteRemount.tsx
@@ -0,0 +1,8 @@
+// components/RouteRemount.tsx
+"use client";
+import { usePathname } from "next/navigation";
+
+export default function RouteRemount({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  return <div key={pathname}>{children}</div>;
+}
